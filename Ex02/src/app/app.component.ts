a86cf0260180f16895dc7cd6632f52a6c4cd3b5c
Mônica Ribeiro <monicaspribeiro@gmail.com>
2023-06-23 22:26:39 -0300

    Lista 2 - módulo 3

---
+++ Ex02/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import Analista from './models/analista';
+import Gestor from './models/gestor';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Ex02';
+
+  public analistas: Analista[] = new Array<Analista>();
+  public gestor: Gestor = new Gestor("Helena", "Gestor de Vendas", 789123, []);
+
+  constructor() {
+    this.analistas.push(new Analista("Amanda", "Analista de Segurança", 123059, this.gestor.nome));
+    this.analistas.push(new Analista("Lucas", "Analista de Dados", 34503, this.gestor.nome));
+    this.analistas.push(new Analista("André", "Analista de Cloud", 792134, this.gestor.nome));
+    this.analistas.push(new Analista("Ana", "Analista de Cyber-Security", 321678, this.gestor.nome));
+    this.analistas.push(new Analista("Mariana", "Analista de Infrasestrutura", 543876, this.gestor.nome));
+    this.analistas.push(new Analista("Roberta", "Analista de Redes", 436932, this.gestor.nome));
+    this.analistas.push(new Analista("Fernando", "Analista de Negócios", 190834, this.gestor.nome));
+    this.analistas.push(new Analista("Diego", "Analista de Sistemas", 579753, this.gestor.nome));
+    this.analistas.push(new Analista("Gustavo", "Analista de Sistemas", 453832, this.gestor.nome));
+    this.gestor.nomeSubordinados = this.analistas.map(analistas => analistas.nome);
+  }
+}
